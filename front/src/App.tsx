31daadd73f0a44fc574218ff1cfba58ceeba360c
Shunki <tajimura0505@gmail.com>
2024-08-02 21:48:26 +0900

    #1 デザインをアプリ色に合わせる。フォーマット

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,91 +1,92 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
-import ChatContainer from './ui/ChatContainer';
-import ChatMessages from './ui/ChatMessages';
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import ChatContainer from "./ui/ChatContainer";
+import ChatMessages from "./ui/ChatMessages";
 // import ChatUser from './ui/ChatUser';
-import DataContextProvider from './data/DataContext';
-import defaultQuestion, { choice } from './data/defaultQuestion';
+import DataContextProvider from "./data/DataContext";
+import defaultQuestion, { choice } from "./data/defaultQuestion";
 // import Loading from './ui/Loading';
-import WritingLoad from './ui/WritingLoad';
-import getRecommendList from './data/getRecommendList';
-import { Box, Button, ButtonGroup, IconButton, TextField } from '@mui/material';
+import WritingLoad from "./ui/WritingLoad";
+import getRecommendList from "./data/getRecommendList";
+import { Box, Button, ButtonGroup, IconButton, TextField } from "@mui/material";
 
-import SendIcon from '@mui/icons-material/Send';
+import SendIcon from "@mui/icons-material/Send";
 
-import younger from './image/younger.png'
-import older from './image/older.png'
-import ChatUser from './ui/ChatUser';
+import younger from "./image/younger.png";
+import older from "./image/older.png";
+import ChatUser from "./ui/ChatUser";
 
 export type recommendList = {
   drink: {
-    name: string,
-    reason:string
-  }, 
+    name: string;
+    reason: string;
+  };
   food: {
-    name: string,
-    reason:string
-  },
-}
+    name: string;
+    reason: string;
+  };
+};
 
 export interface Question {
   id: number;
-  key:string;
+  key: string;
   question: string;
   list: string[];
   option: Boolean;
   answer: string;
-  example:string[];
+  example: string[];
 }
 
 function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
+      {"Copyright © "}
       <Link color="inherit" href="#">
         nihonshu-shimai
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}.
     </Typography>
   );
 }
 
 function App() {
-
-  const [currentQuestion, setCurrentQuestion] = React.useState<Question>(choice[0]);
+  const [currentQuestion, setCurrentQuestion] = React.useState<Question>(
+    choice[0]
+  );
 
   const [chatHistory, setChatHistory] = React.useState<Question[]>([]);
 
-  const [renderList, setRenderList] = React.useState<string[]>([])
+  const [renderList, setRenderList] = React.useState<string[]>([]);
 
-  const [triggerQuestion, setTriggerQuestion] = React.useState<Boolean>(false)
+  const [triggerQuestion, setTriggerQuestion] = React.useState<Boolean>(false);
 
-  const [loading, setLoading] = React.useState<Boolean>(false)
+  const [loading, setLoading] = React.useState<Boolean>(false);
 
-  const [recommend, setRecommend] = React.useState<recommendList>()
-  const [getRecommend, setGetRecommend] = React.useState<Boolean>(false)
-  const [gotRecommend, setGotRecommend] = React.useState<Boolean>(false)
+  const [recommend, setRecommend] = React.useState<recommendList>();
+  const [getRecommend, setGetRecommend] = React.useState<Boolean>(false);
+  const [gotRecommend, setGotRecommend] = React.useState<Boolean>(false);
 
-  const [buttonClickable, setButtonClickable] = React.useState<Boolean>(false)
+  const [buttonClickable, setButtonClickable] = React.useState<Boolean>(false);
 
   // テンプレ会話の順番表示
   React.useEffect(() => {
     defaultQuestion.forEach((item, index) => {
       setTimeout(() => {
-        setRenderList(prevList => [...prevList, item])
-      }, 1*1000*index+Math.floor(Math.random() * 500))
-    })
-  }, [])
+        setRenderList((prevList) => [...prevList, item]);
+      }, 1 * 1000 * index + Math.floor(Math.random() * 500));
+    });
+  }, []);
   // 最初の質問表示
   React.useEffect(() => {
     if (renderList.length === defaultQuestion.length) {
       setTimeout(() => {
-        setTriggerQuestion(true) 
-      }, 1*1000)
+        setTriggerQuestion(true);
+      }, 1 * 1000);
     }
-  }, [renderList])
-  
+  }, [renderList]);
+
   // Inputの入力反映
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setCurrentQuestion({
@@ -99,7 +100,7 @@ function App() {
     const activeElement = document.activeElement as HTMLElement;
     // フォーカスを外す
     activeElement.blur();
-    nextQuestion(currentQuestion.answer)
+    nextQuestion(currentQuestion.answer);
   };
   // 選択肢ボタンの選択
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -107,42 +108,47 @@ function App() {
       ...currentQuestion,
       answer: event.currentTarget.value,
     });
-    nextQuestion(event.currentTarget.value)
-  }
+    nextQuestion(event.currentTarget.value);
+  };
   // 次の質問に
-  const nextQuestion = async (newAnswer: string) =>{
+  const nextQuestion = async (newAnswer: string) => {
     // 次の質問を一旦非表示に
-    setTriggerQuestion(false)
+    setTriggerQuestion(false);
     // 回答をオブジェクトリストに格納
     setChatHistory([
-      ...chatHistory, {
+      ...chatHistory,
+      {
         ...currentQuestion,
         answer: newAnswer,
-      }
+      },
     ]);
     // Loading ON
-    setLoading(true)
-    await new Promise((resolve) => setTimeout(resolve, Math.floor(Math.random() * 1500)));
+    setLoading(true);
+    await new Promise((resolve) =>
+      setTimeout(resolve, Math.floor(Math.random() * 1500))
+    );
     // 次の質問を表示
-    if(chatHistory.length < choice.length - 1 ) {
+    if (chatHistory.length < choice.length - 1) {
       const nextQuestionIndex = currentQuestion.id;
       setCurrentQuestion(choice[nextQuestionIndex]);
       // Loading解除、次の質問を再表示
-      setLoading(false)
-      setTriggerQuestion(true)
+      setLoading(false);
+      setTriggerQuestion(true);
     } else {
-      setTriggerQuestion(false)
-      setGetRecommend(true)
-      setLoading(true)
-    }  
-  }
+      setTriggerQuestion(false);
+      setGetRecommend(true);
+      setLoading(true);
+    }
+  };
 
   // スクロール
-  React.useEffect(()=>{
+  React.useEffect(() => {
     try {
-      document.getElementsByClassName('question')[0]!.scrollIntoView({ behavior: "smooth"});  
+      document
+        .getElementsByClassName("question")[0]!
+        .scrollIntoView({ behavior: "smooth" });
     } catch (error) {}
-  },[currentQuestion, gotRecommend, loading])
+  }, [currentQuestion, gotRecommend, loading]);
 
   // APIをたたく
   function useAsyncEffect(effect: () => Promise<void>, deps?: any[]) {
@@ -151,102 +157,150 @@ function App() {
     }, deps);
   }
   useAsyncEffect(async () => {
-    if (getRecommend){
-      const res = await getRecommendList(chatHistory)
-      setRecommend(res)
-      setLoading(false)
-      setGotRecommend(true)
+    if (getRecommend) {
+      const res = await getRecommendList(chatHistory);
+      setRecommend(res);
+      setLoading(false);
+      setGotRecommend(true);
     }
   }, [getRecommend]);
 
-  React.useEffect(()=>{
-    if (loading){
-      setButtonClickable(false)
+  React.useEffect(() => {
+    if (loading) {
+      setButtonClickable(false);
     }
-  },[loading])
+  }, [loading]);
 
-  const handleClickRetry = () =>{
-    window.location.reload()
-  }
+  const handleClickRetry = () => {
+    window.location.reload();
+  };
 
   return (
     <DataContextProvider>
-    <Container maxWidth="sm" sx={{ px: 0 }}>
-      <ChatContainer>
-        <>
-          {/* 最初のテンプレ会話 */}
-          {renderList.map((item: string, index: number) => {
+      <Container maxWidth="sm" sx={{ px: 0 }}>
+        <ChatContainer>
+          <>
+            {/* 最初のテンプレ会話 */}
+            {renderList.map((item: string, index: number) => {
               if (index === 2) {
-                return <ChatMessages key={index} src={younger}><>{item}</></ChatMessages>
+                return (
+                  <ChatMessages key={index} src={younger}>
+                    <>{item}</>
+                  </ChatMessages>
+                );
               } else {
-                return <ChatMessages key={index} src={older}><>{item}</></ChatMessages>
+                return (
+                  <ChatMessages key={index} src={older}>
+                    <>{item}</>
+                  </ChatMessages>
+                );
               }
             })}
-            
-          {/* 質問回答履歴の表示 */}
-          {chatHistory.map((chat, index) => (
-            <div key={index}>
-              <ChatMessages src={older}><>{chat?.question}</></ChatMessages>
-              {!chat?.option && 
-                <>
-                  <ChatMessages src={younger}>
-                    <>「{chat?.example[0]}」とか「{chat?.example[1]}」とか</>
-                  </ChatMessages>
-                  <ChatMessages src={younger}>
-                    <>↓に入力してね</>
-                  </ChatMessages>
-                </>
-              }
-              <ChatUser text={chat?.answer} />
-            </div>
-          ))}
-          {/* 次の質問と選択肢の表示 */}
-          {triggerQuestion && 
-            <div className='question'>
-              <ChatMessages src={older}><>{currentQuestion?.question}</></ChatMessages>
-              {currentQuestion?.option ? 
-                  <ButtonGroup color="error" variant='contained'>
-                      {currentQuestion?.list.map((item) => {
-                          return (
-                              <Button key={item} onClick={handleClick} value={item}>{item}</Button>
-                          )
-                      })}
-                  </ButtonGroup>:
+
+            {/* 質問回答履歴の表示 */}
+            {chatHistory.map((chat, index) => (
+              <div key={index}>
+                <ChatMessages src={older}>
+                  <>{chat?.question}</>
+                </ChatMessages>
+                {!chat?.option && (
                   <>
                     <ChatMessages src={younger}>
-                      <>「{currentQuestion?.example[0]}」とか「{currentQuestion?.example[1]}」とか</>
+                      <>
+                        「{chat?.example[0]}」とか「{chat?.example[1]}」とか
+                      </>
                     </ChatMessages>
                     <ChatMessages src={younger}>
                       <>↓に入力してね</>
                     </ChatMessages>
                   </>
-              }
-            </div>
-            }
-          {/* Loading表示 */}
-          {loading && <div className='question'><WritingLoad /></div>}
-          {/* おススメの表示 */}
-          {gotRecommend && 
-            <div className='question'>
-              <ChatMessages src={older}><>こちらをおススメします！</></ChatMessages>
-              <ChatMessages src={older}><>{recommend?.drink.name}</></ChatMessages>
-              <ChatMessages src={older}><>{recommend?.drink.reason}</></ChatMessages>
-              <ChatMessages src={younger}><>{recommend?.food.name}</></ChatMessages>
-              <ChatMessages src={younger}><>{recommend?.food.reason}</></ChatMessages>
-              <Button variant='contained' onClick={handleClickRetry}>もう一度聞いてみる</Button>
-            </div>            
-          }
-      </>
+                )}
+                <ChatUser text={chat?.answer} />
+              </div>
+            ))}
+            {/* 次の質問と選択肢の表示 */}
+            {triggerQuestion && (
+              <div className="question">
+                <ChatMessages src={older}>
+                  <>{currentQuestion?.question}</>
+                </ChatMessages>
+                {currentQuestion?.option ? (
+                  <ButtonGroup color="primary" variant="contained">
+                    {currentQuestion?.list.map((item) => {
+                      return (
+                        <Button key={item} onClick={handleClick} value={item}>
+                          {item}
+                        </Button>
+                      );
+                    })}
+                  </ButtonGroup>
+                ) : (
+                  <>
+                    <ChatMessages src={younger}>
+                      <>
+                        「{currentQuestion?.example[0]}」とか「
+                        {currentQuestion?.example[1]}」とか
+                      </>
+                    </ChatMessages>
+                    <ChatMessages src={younger}>
+                      <>↓に入力してね</>
+                    </ChatMessages>
+                  </>
+                )}
+              </div>
+            )}
+            {/* Loading表示 */}
+            {loading && (
+              <div className="question">
+                <WritingLoad />
+              </div>
+            )}
+            {/* おススメの表示 */}
+            {gotRecommend && (
+              <div className="question">
+                <ChatMessages src={older}>
+                  <>こちらをおススメします！</>
+                </ChatMessages>
+                <ChatMessages src={older}>
+                  <>{recommend?.drink.name}</>
+                </ChatMessages>
+                <ChatMessages src={older}>
+                  <>{recommend?.drink.reason}</>
+                </ChatMessages>
+                <ChatMessages src={younger}>
+                  <>{recommend?.food.name}</>
+                </ChatMessages>
+                <ChatMessages src={younger}>
+                  <>{recommend?.food.reason}</>
+                </ChatMessages>
+                <Button variant="contained" onClick={handleClickRetry}>
+                  もう一度聞いてみる
+                </Button>
+              </div>
+            )}
+          </>
         </ChatContainer>
-      <form onSubmit={handleFormSubmit}>
-      <Box sx={{width:'100%', display:'flex', backgroundColor:'#fff', m:0}}>
-        <TextField variant='standard' sx={{flexGrow : 1}} value={currentQuestion?.answer} onChange={handleInputChange}/>
-        <IconButton type='submit' >
-          <SendIcon />
-        </IconButton>
-      </Box>
-      </form>
-      <Copyright />
+        <form onSubmit={handleFormSubmit}>
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              backgroundColor: "#fff",
+              m: 0,
+            }}
+          >
+            <TextField
+              variant="standard"
+              sx={{ flexGrow: 1 }}
+              value={currentQuestion?.answer}
+              onChange={handleInputChange}
+            />
+            <IconButton type="submit">
+              <SendIcon />
+            </IconButton>
+          </Box>
+        </form>
+        <Copyright />
       </Container>
     </DataContextProvider>
   );
--- front/src/ui/Choice.tsx
+++ front/src/ui/Choice.tsx
@@ -1,70 +1,83 @@
-import React, { useContext, useEffect, useState } from 'react'
-import styles from './choice.module.css'
-import { Button, ButtonGroup } from '@mui/material'
-import { DataContext } from '../data/DataContext'
-import ChatMessages from './ChatMessages'
-import ChatUser from './ChatUser'
+import React, { useContext, useEffect, useState } from "react";
+import styles from "./choice.module.css";
+import { Button, ButtonGroup } from "@mui/material";
+import { DataContext } from "../data/DataContext";
+import ChatMessages from "./ChatMessages";
+import ChatUser from "./ChatUser";
 
-import older from '../image/older.png'
-import younger from '../image/younger.png'
+import older from "../image/older.png";
+import younger from "../image/younger.png";
 
 type Props = {
-    choice: {
-        list: string[],
-        key: string,
-        question: string,
-        index: number,
-        option: boolean,
-        example: string[]
-    }
-    setQuestionNumber:React.Dispatch<React.SetStateAction<number[]>>
-}
+  choice: {
+    list: string[];
+    key: string;
+    question: string;
+    index: number;
+    option: boolean;
+    example: string[];
+  };
+  setQuestionNumber: React.Dispatch<React.SetStateAction<number[]>>;
+};
 
 const Choice = ({ choice, setQuestionNumber }: Props) => {
-    const { choiceAnswer, setChoiceAnswer, clickSubmit, setClickSubmit, inputText} = useContext(DataContext)
-    const [renderAnswer, setRenderAnswer] = useState(false)
-    const [answer, setAnswer] = useState<string>("")
-    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        setAnswer(event.currentTarget.value)
-        setChoiceAnswer({
-            ...choiceAnswer,
-            [choice.key]: event.currentTarget.value,
-        })
-        setRenderAnswer(true)
-        setQuestionNumber(prev => [...prev, choice.index])
+  const {
+    choiceAnswer,
+    setChoiceAnswer,
+    clickSubmit,
+    setClickSubmit,
+    inputText,
+  } = useContext(DataContext);
+  const [renderAnswer, setRenderAnswer] = useState(false);
+  const [answer, setAnswer] = useState<string>("");
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnswer(event.currentTarget.value);
+    setChoiceAnswer({
+      ...choiceAnswer,
+      [choice.key]: event.currentTarget.value,
+    });
+    setRenderAnswer(true);
+    setQuestionNumber((prev) => [...prev, choice.index]);
+  };
+
+  useEffect(() => {
+    if (clickSubmit) {
+      setAnswer(inputText);
+      setRenderAnswer(true);
+      setClickSubmit(false);
     }
+  }, [clickSubmit]);
 
-    useEffect(()=>{
-        if (clickSubmit){
-            setAnswer(inputText)
-            setRenderAnswer(true)
-            setClickSubmit(false)
-        }
-    },[clickSubmit])
-
-    return (
+  return (
+    <>
+      <ChatMessages src={older}>
+        <>{choice.question}</>
+      </ChatMessages>
+      {!choice.option && (
         <>
-            <ChatMessages src={older}><>{choice.question}</></ChatMessages>
-            {!choice.option && 
-                <>
-                    <ChatMessages src={younger}><>例）{choice.example.map(item=>item+" ")}</></ChatMessages>
-                    <ChatMessages src={younger}><>下に入力して送信ボタンを押してね</></ChatMessages>
-                </>
-            }
-            {!renderAnswer &&
-                <div className={styles.overlay}>
-                    <ButtonGroup color="error" variant='contained'>
-                        {choice.list.map((item) => {
-                            return (
-                                <Button key={item} onClick={handleClick} value={item}>{item}</Button>
-                            )
-                        })}
-                    </ButtonGroup>
-                </div>}
-            {renderAnswer && <ChatUser text={answer} />}
-            {!choice.option && renderAnswer && <ChatUser text={answer} />}
+          <ChatMessages src={younger}>
+            <>例）{choice.example.map((item) => item + " ")}</>
+          </ChatMessages>
+          <ChatMessages src={younger}>
+            <>下に入力して送信ボタンを押してね</>
+          </ChatMessages>
         </>
-  )
-}
+      )}
+      {!renderAnswer && (
+        <ButtonGroup color="primary" variant="outlined">
+          {choice.list.map((item) => {
+            return (
+              <Button key={item} onClick={handleClick} value={item}>
+                {item}
+              </Button>
+            );
+          })}
+        </ButtonGroup>
+      )}
+      {renderAnswer && <ChatUser text={answer} />}
+      {!choice.option && renderAnswer && <ChatUser text={answer} />}
+    </>
+  );
+};
 
-export default Choice
+export default Choice;
